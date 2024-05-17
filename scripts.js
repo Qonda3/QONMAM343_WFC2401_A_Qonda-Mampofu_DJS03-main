643b7409f4f8ad52fc2b5b303fd7efbe41dfe204
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-05-17 14:35:08 +0200

    add on the addEventListner

--- scripts.js
+++ scripts.js
@@ -107,6 +107,68 @@ function addEventListeners() {
         handleThemeChange(theme)
         document.querySelector('[data-settings-overlay]').open = false
     })
+
+    document.querySelector('[data-search-form]').addEventListener('submit', (event) => {
+        event.preventDefault()
+        const formData = new FormData(event.target)
+        const filters = Object.fromEntries(formData)
+        const result = []
+    
+        for (const book of books) {
+            let genreMatch = filters.genre === 'any'
+    
+            for (const singleGenre of book.genres) {
+                if (genreMatch) break;
+                if (singleGenre === filters.genre) { genreMatch = true }
+            }
+    
+            if (
+                (filters.title.trim() === '' || book.title.toLowerCase().includes(filters.title.toLowerCase())) && 
+                (filters.author === 'any' || book.author === filters.author) && 
+                genreMatch
+            ) {
+                result.push(book)
+            }
+        }
+    
+        currentPage = 1;
+        filteredBooks = result;
+    
+        if (result.length < 1) {
+            document.querySelector('[data-list-message]').classList.add('list__message_show')
+        } else {
+            document.querySelector('[data-list-message]').classList.remove('list__message_show')
+        }
+    
+        document.querySelector('[data-list-items]').innerHTML = ''
+        const newItems = document.createDocumentFragment()
+    
+        for (const book of result.slice(0, BOOKS_PER_PAGE)) {
+            newItems.appendChild(createBookElement(book))
+        }
+        
+        document.querySelector('[data-list-items]').appendChild(newItems)
+        document.querySelector('[data-list-button]').disabled = (filteredBooks.length - (currentPage * BOOKS_PER_PAGE)) < 1
+    
+        document.querySelector('[data-list-button]').innerHTML = `
+            <span>Show more</span>
+            <span class="list__remaining"> (${(filteredBooks.length - (currentPage * BOOKS_PER_PAGE)) > 0 ? (filteredBooks.length - (currentPage * BOOKS_PER_PAGE)) : 0})</span>
+        `
+    
+        window.scrollTo({top: 0, behavior: 'smooth'});
+        document.querySelector('[data-search-overlay]').open = false
+    })
+    
+    document.querySelector('[data-list-button]').addEventListener('click', () => {
+        const fragment = document.createDocumentFragment()
+    
+        for (const book of filteredBooks.slice(currentPage * BOOKS_PER_PAGE, (currentPage + 1) * BOOKS_PER_PAGE)) {
+            fragment.appendChild(createBookElement(book))
+        }
+    
+        document.querySelector('[data-list-items]').appendChild(fragment)
+        currentPage += 1
+    })
 }
 
 document.querySelector('[data-search-form]').addEventListener('submit', (event) => {
